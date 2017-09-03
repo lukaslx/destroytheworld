58c9b4c101cb1f16be643ebdf70160b95346b367
Aleksander Łukasiewicz <lukasiewicz.aleksander@gmail.com>
2017-09-03 13:55:38 +0200

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Aleksander Łukasiewicz on 03/09/2017.
+//  Copyright © 2017 Aleksander Łukasiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
